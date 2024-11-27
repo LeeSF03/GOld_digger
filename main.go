2b805d03e5bede637e4f9d3629b54395e02b463d
LeeSF03 <shuenfeilee@gmail.com>
2024-11-28 00:26:58 +0800

    chore: add interactive console

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"GOld_digger/repl"
+	"fmt"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is LeeSF's programming language!\nHave fun digging!\n", user.Username)
+
+	repl.Start(os.Stdin, os.Stdout)
+}
