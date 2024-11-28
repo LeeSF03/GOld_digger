c426cc98d5da73e2b19268ddc4ca28250efc0680
LeeSF03 <shuenfeilee@gmail.com>
2024-11-29 00:22:45 +0800

    chore: add parser types and initializor

--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,30 @@
 package parser
+
+import (
+	"GOld_digger/ast"
+	"GOld_digger/lexer"
+	"GOld_digger/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token // current Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
