d240e38cc857f499c546f2d51f5e99b10b3286f2
LeeSF03 <shuenfeilee@gmail.com>
2024-11-29 00:16:30 +0800

    chore: add let statement struct and function

--- ast/ast.go
+++ ast/ast.go
@@ -30,6 +30,15 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
 type Identifier struct {
 	Token token.Token
 	Value string
